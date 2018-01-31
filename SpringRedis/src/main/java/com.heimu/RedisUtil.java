e7bce9b5fc9dc67f7959326bfb40e6867b81a893
linmingyun <813762318@qq.com>
2018-01-31 23:24:51 +0800

    SpringRedis

--- SpringRedis/src/main/java/com.heimu/RedisUtil.java
+++ SpringRedis/src/main/java/com.heimu/RedisUtil.java
@@ -10,7 +10,7 @@ import org.springframework.util.CollectionUtils;
 
 /**
  * 基于spring和redis的redisTemplate工具类
- * 针对所有的hash 都是以h开头的方法
+ * 针对所有的Hash 都是以h开头的方法
  * 针对所有的Set 都是以s开头的方法 不含通用方法
  * 针对所有的List 都是以l开头的方法
  */
